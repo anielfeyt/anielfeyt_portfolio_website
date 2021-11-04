c4cd401da857c6f486f837c660f6c0e8164d0cdf
Aniel-Feyt <anielfeyt@gmail.com>
2021-11-04 20:59:22 +0200

    Added html.js

---
+++ src/html.js
@@ -0,0 +1,44 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function HTML (props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+        <script id="mcjs"
+          dangerouslySetInnerHTML={{
+            __html: `
+    !function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/19dcbc70aa5cde569dddebd0b/b3b9e6857832e2bf6f6709e81.js");
+        `,
+          }}
+        />
+
+      </body>
+    </html>
+  );
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,6 @@ const IndexPage = () => {
     <React.Fragment>
       <Helmet>
         <title>Aniel Feyt</title>
-        <script id="mcjs">!function(c,h,i,m,p){m = c.createElement(h), p = c.getElementsByTagName(h)[0], m.async = 1, m.src = i, p.parentNode.insertBefore(m, p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/19dcbc70aa5cde569dddebd0b/b3b9e6857832e2bf6f6709e81.js");</script>
       </Helmet>
       <MainNav />
       <main>
